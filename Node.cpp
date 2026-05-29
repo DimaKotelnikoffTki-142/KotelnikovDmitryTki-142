1e11d1d5726d8fb85ead127882419865fab53c0c
DimaKotelnikoffTki-142 <dimylya130707@gmail.com>
2026-05-29 18:05:16 +0300

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,12 +1,12 @@
 #include "Node.h"
 
-Node::Node(int value, Node* next)
+Node::Node(const int value, const Node* next)
     : m_value(value), m_next(next)
 {
 }
 
 int  Node::value() const { return m_value; }
 int& Node::valueRef() { return m_value; }
-void Node::setValue(int v) { m_value = v; }
+void Node::setValue(const int v) { m_value = v; }
 Node* Node::next() const { return m_next; }
-void  Node::setNext(Node* n) { m_next = n; }
+void  Node::setNext(const Node* n) { m_next = n; }
